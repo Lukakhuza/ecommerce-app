3e3c748b8332b380c03fe9de32b0fb026aff1ab5
lukakhuza <lukakhuza7@gmail.com>
2026-06-03 13:16:05 -0400

    addPaymentMethod migrated to AWS Lambda(1)

--- src/api/checkout.api.ts
+++ src/api/checkout.api.ts
@@ -2,13 +2,16 @@ import { url } from './client';
 
 // Migrated to AWS Lambda ✅
 export const addPaymentMethod = async (paymentMethodData: Object) => {
-  const response = await fetch(url + '/checkout/add-payment-method', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    'https://oyrhq868lf.execute-api.us-east-1.amazonaws.com/add-payment-method',
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(paymentMethodData),
     },
-    body: JSON.stringify(paymentMethodData),
-  });
+  );
   const resData = await response.json();
   return resData;
 };
